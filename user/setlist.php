4949484515545f335f4777eb54300d32538ff670
emon-1111 <maharjanemon@gmail.com>
2026-02-11 22:07:32 +0545

    favicon

--- user/setlist.php
+++ user/setlist.php
@@ -27,7 +27,7 @@ $result = $stmt->get_result();
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Playlists - LyricScroll</title>
- <link rel="icon" type="image/png" href="../favicon.png">
+<link rel="icon" type="image/png" href="../favicon.png">
 <link rel="stylesheet" href="../frontend/assets/css/user.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 <style>
